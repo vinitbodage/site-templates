1995cb5008180f718d38d6715f852e741910efb6
Vinit Maruti Bodage <VinitMaruti.Bodage@nttdata.com>
2026-08-17 18:41:59 +0530

    updated header

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -204,6 +204,12 @@ export default async function decorate(block) {
 
   const navSections = nav.querySelector('.nav-sections');
   if (navSections) {
+    // nav links authored as the sole content of a paragraph get auto-decorated
+    // as buttons; nav links should always render as plain text links
+    navSections.querySelectorAll('a.button').forEach((a) => {
+      a.classList.remove('button', 'primary', 'secondary');
+      a.closest('.button-container')?.classList.remove('button-container');
+    });
     navSections.querySelectorAll(':scope .default-content-wrapper > ul > li').forEach((navSection) => {
       if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
       navSection.addEventListener('click', () => {
